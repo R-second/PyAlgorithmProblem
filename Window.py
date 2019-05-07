73c81d37703a3f3f87a85b01dc3ea6245181fa29
山田吏月 <R-second@yamadarigatsu-no-MacBook-Air.local>
2019-05-07 20:11:41 +0900

    try commit

---
+++ Window.py
@@ -0,0 +1,49 @@
+# Window.pyw
+import tkinter
+
+# 上位クラスWindow（MainWindowとSubWindowを継承させる）
+class Window(tkinter.Frame):
+
+    def __init__(self,parent):
+        super(Window, self).__init__(parent)
+        self.parent = parent
+        self.grid(row=0, column=0)
+    
+    # windowを正常に閉じるための関数 quit
+    def quit(self, event=None):
+        self.parent.destroy()
+
+# Windowクラスを継承したMainWindowクラス
+class MainWindow(Window):
+
+    def __init__(self, parent):
+        super(MainWindow, self).__init__(parent)
+
+        #Widgets
+        #ボタンを６つ用意する（クリックされたら、subWindow関数の引数にボタン番を渡してを実行）
+        self.btn1 = tkinter.Button(self, text="素数判定＋素因数分解", command=lambda:self.subWindow(0))
+        self.btn2 = tkinter.Button(self, text="フィボナッチ数列の出力", command=lambda:self.subWindow(1))
+        self.btn3 = tkinter.Button(self, text="最大公約数出力", command=lambda:self.subWindow(2))
+        self.btn4 = tkinter.Button(self, text="最大値/最小値判定", command=lambda:self.subWindow(3))
+        self.btn5 = tkinter.Button(self, text="ソート", command=lambda:self.subWindow(4))
+        self.btn6 = tkinter.Button(self, text="万年カレンダー", command=lambda:self.subWindow(5))
+
+        #Layout
+        self.btn1.grid(row=0, column=0, padx=2, pady=2, sticky=tkinter.W)
+        self.btn2.grid(row=1, column=0, padx=2, pady=2, sticky=tkinter.W)
+        self.btn3.grid(row=2, column=0, padx=2, pady=2, sticky=tkinter.W)
+        self.btn4.grid(row=3, column=0, padx=2, pady=2, sticky=tkinter.W)
+        self.btn5.grid(row=4, column=0, padx=2, pady=2, sticky=tkinter.W)
+
+    # ボタンがクリックされた時の処理を実行するsubWindow関数
+    def subWindow(self, num):
+        application2 = tkinter.Tk()
+        application2.title("subWindow")
+        subWindow = SubWindow(application2, num) # SubWindowクラスをsubWindow変数に代入
+        application2.protocol("WM_DELETE_WINDOW", subWindow.quit)
+        application2.mainloop()
+
+    
+class SubWindow(Window):
+    def __init__(self, parent, num):
+        super(MainWindow, self).__init__(parent)
---
+++ main.py
@@ -0,0 +1,52 @@
+# main.py
+import tkinter
+from Window import Window, MainWindow, SubWindow
+
+# main処理の関数
+def main():
+    application = tkinter.Tk()
+    application.title("mainWindow")
+    window = MainWindow(application)
+    application.protocol("WM_DELETE_WINDOW", window.quit)
+    application.mainloop()
+
+if __name__ == '__main__':
+    main()
+
+
+# def main():
+#     print("このプログラムでは以下のアルゴリズムを試すことができます。")
+#     print("1. 素数判定＋素因数分解")
+#     print("2. フィボナッチ数列の出力")
+#     print("3. 最大公約数の出力")
+#     print("4. 最大値・最小値の判定")
+#     print("5. ソート")
+#     print("6. 万年カレンダー")
+#     st = input("試したいアルゴリズムの数字を半角で入力してください。（終了したい場合はq）＞")
+
+#     while st == "q" or int(st)>7:
+#         print("不適切な文字が入力されました。半角数字かqを入力してください。")
+#         st = input("試したいアルゴリズムの数字を半角で入力してください。（終了したい場合はq）＞")
+    
+#     num = int(st)
+#     while num<7:
+#         if num==1:
+#             print("1を実行します。")
+#             break
+#         elif num==2:
+#             print("2を実行します。")
+#             break
+#         elif num==3:
+#             print("3を実行します。")
+#             break
+#         elif num==4:
+#             print("4を実行します。")
+#             break
+#         elif num==5:
+#             print("5を実行します。")
+#             break
+#         elif num==6:
+#             print("6を実行します。")
+#             break
+
+# main()
